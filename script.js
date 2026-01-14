a39b504fa40c6723d8476f05d75c3352c4ce22c5
Dav1Gomes <d4v1gomes@gmail.com>
2026-01-14 03:16:39 -0300

    feat: adicionando fotos e textos aos módulos e ajustando o menu

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-function gerarNumeroAleatorio() {
-  const numero = Math.floor(Math.random() * 6) + 1;
-  console.log(`Número gerado: ${numero}`);
-  return numero;
-}
 
-document.addEventListener('DOMContentLoaded', function() {
-  console.log('Página carregada!');
-  gerarNumeroAleatorio();
-});
-
-document.addEventListener('click', function(evento) {
-  if (evento.target.classList.contains('btn-menu')) {
-    console.log('Menu clicado!');
-    gerarNumeroAleatorio();
-  }
-});
